fd5354645768576b103cf9061db38413d481d6e6
MrShanks <simonestaffoli@gmail.com>
2024-02-05 19:13:54 +0100

    feat: add post request

---
+++ cmd/main.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/MrShanks/pizza-delivery/pkg/kitchen/restaurant/order"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+const url = "http://localhost:3010/kitchen"
+
+type model struct {
+	choices  []string         // items on the to-do list
+	cursor   int              // which to-do list item our cursor is pointing at
+	selected map[int]struct{} // which to-do items are selected
+	status   int
+	err      error
+}
+
+func initialModel() model {
+	return model{
+		choices:  []string{"Margherita", "Capricciosa", "Diavola", "Calabra"},
+		selected: make(map[int]struct{}),
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	// Just return `nil`, which means "no I/O right now, please."
+	return checkServer
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	// Is it a key press?
+	case tea.KeyMsg:
+
+		// Cool, what was the actual key pressed?
+		switch msg.String() {
+
+		// These keys should exit the program.
+		case "ctrl+c", "q":
+			return m, tea.Quit
+
+		// The "up" and "k" keys move the cursor up
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+
+		// The "down" and "j" keys move the cursor down
+		case "down", "j":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+
+		// The "enter" key and the spacebar (a literal space) toggle
+		// the selected state for the item that the cursor is pointing at.
+		case "enter", " ":
+			_, ok := m.selected[m.cursor]
+			if ok {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = struct{}{}
+			}
+		}
+	}
+
+	// Return the updated model to the Bubble Tea runtime for processing.
+	// Note that we're not returning a command.
+	return m, nil
+}
+
+func (m model) View() string {
+	// The header
+	s := "Welcome to pizza-delivery restaurant, what pizza do you want?\n\n"
+
+	// Iterate over our choices
+	for i, choice := range m.choices {
+
+		// Is the cursor pointing at this choice?
+		cursor := " " // no cursor
+		if m.cursor == i {
+			cursor = ">" // cursor!
+		}
+
+		// Is this choice selected?
+		checked := " " // not selected
+		if _, ok := m.selected[i]; ok {
+			checked = "x" // selected!
+		}
+
+		// Render the row
+		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+	}
+
+	// The footer
+	s += "\nPress q to quit.\n"
+
+	// Send the UI for rendering
+	return s
+}
+
+func main() {
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
+}
+
+func checkServer() tea.Msg {
+
+	// Create an HTTP client and make a GET request.
+	c := &http.Client{Timeout: 10 * time.Second}
+	ordJSON, err := json.Marshal(order.NewOrder())
+	if err != nil {
+		log.Fatal("Error marshalling JSON: ", err)
+	}
+	res, err := c.Post(url, "application/json", bytes.NewBuffer(ordJSON))
+
+	if err != nil {
+		// There was an error making our request. Wrap the error we received
+		// in a message and return it.
+		return errMsg{err}
+	}
+	// We received a response from the server. Return the HTTP status code
+	// as a message.
+	return statusMsg(res.StatusCode)
+}
+
+type statusMsg int
+
+type errMsg struct{ err error }
+
+// For messages that contain errors it's often handy to also implement the
+// error interface on the message.
+func (e errMsg) Error() string { return e.err.Error() }
